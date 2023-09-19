435c1d9b79118cdb3f7df2c52d752c4691eafce0
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 16:01:04 +0530

    Added onclicks for cat images

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function switchOff() {
     document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src = "";
 }
 
 function switchOn() {
     document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src = "";
 }
