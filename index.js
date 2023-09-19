bead8f8e7bb7ca8c6776e16eff664d550e5c9824
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 15:54:58 +0530

    Added functions for switch off and on and made it dynamic.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function switchOff() {
-
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
 }
 
 function switchOn() {
-
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
 }
