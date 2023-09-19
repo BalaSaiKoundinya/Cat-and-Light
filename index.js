79dbb3026397fcc5896b76e4a7253166944454d3
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 16:40:52 +0530

    minor changes in colors in js file

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function switchOff() {
     document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
     document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
     document.getElementById("switchStatus").textContent = "SwitchedOff";
-    document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9"; //red
-    document.getElementById("onSwitch").style.backgroundColor = "#22c55e"; // grey
+    document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9"; //grey
+    document.getElementById("onSwitch").style.backgroundColor = "#22c55e"; // green
 }
 
 function switchOn() {
     document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
     document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
     document.getElementById("switchStatus").textContent = "SwitchedOn";
-    document.getElementById("offSwitch").style.backgroundColor = "#e12d39"; //grey
-    document.getElementById("onSwitch").style.backgroundColor = "#cbd2d9"; //green
+    document.getElementById("offSwitch").style.backgroundColor = "#e12d39"; //red
+    document.getElementById("onSwitch").style.backgroundColor = "#cbd2d9"; //grey
 }
